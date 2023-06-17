5b55a28a245759ccdd3aa372d768975c301aa1d3
JAEHO JEONG <e0960435@u.nus.edu>
2023-06-17 17:00:33 +0800

    Tried adding Calendar page, pending error

--- components/Navbar/UserMenu.tsx
+++ components/Navbar/UserMenu.tsx
@@ -1,35 +1,36 @@
-'use client'
+"use client";
 
-import React, { useCallback } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
-import Avatar from './Avatar'
-import { useState } from 'react'
-import MenuItem from './MenuItem'
-import useRegisterModal from '../../app/hooks/useRegisterModal'
-import useLoginModal from '../../app/hooks/useLoginModal'
-import { User } from '@prisma/client'
-import { signOut } from 'next-auth/react'
+import React, { useCallback } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "./Avatar";
+import { useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "../../app/hooks/useRegisterModal";
+import useLoginModal from "../../app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 interface UserMenuProps {
-    currentUser?: User | null
+  currentUser?: User | null;
 }
 
-const UserMenu: React.FC<UserMenuProps> = ({
-    currentUser
-}) => {
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
-    const [isOpen, setIsOpen] = useState(false);
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
-    const toggleOpen = useCallback(() => {
-        setIsOpen((value) => !value)
-    }, [setIsOpen])
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, [setIsOpen]);
+
   return (
-    <div className='relative'>
-        <div className='flex flex-row items-center gap-3'>
-            <div 
-                onClick = {() => {}}
-                className='
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => router.push("/queue")}
+          className="
                     hidden
                     md:block
                     text-sm
@@ -39,13 +40,14 @@ const UserMenu: React.FC<UserMenuProps> = ({
                     rounded-full
                     hover:bg-neutral-100
                     transition
-                    cursor-pointer'>
-                        Queue
-            </div>
+                    cursor-pointer"
+        >
+          Queue
+        </div>
 
-            <div
-                onClick = {toggleOpen}
-                className='
+        <div
+          onClick={toggleOpen}
+          className="
                     p-4
                     md:py-1
                     md:px-2
@@ -58,16 +60,17 @@ const UserMenu: React.FC<UserMenuProps> = ({
                     rounded-full
                     cursor-pointer
                     hover:shadow-md
-                    transition'>
-                <AiOutlineMenu />
-                <div className='hidden md:block'>
-                    <Avatar />
-                </div>
-            </div>
+                    transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
-        {isOpen && (
-            <div
-                className='
+      </div>
+      {isOpen && (
+        <div
+          className="
                     absolute
                     rounded-xl
                     shadow-md
@@ -77,38 +80,26 @@ const UserMenu: React.FC<UserMenuProps> = ({
                     overflow-hidden
                     right-0
                     top-12
-                    text-sm'
-            >
-                <div className='flex flex-col cursor-pointer'>
-                    {currentUser ? (
-                        <>
-                        <MenuItem 
-                            onClick={() => {}}
-                            label = "Edit Profile"
-                        />
-                        <hr />
-                        <MenuItem 
-                            onClick={() => signOut()}
-                            label = "Log Out"
-                        />
-                    </>
-                    ) : (
-                        <>
-                        <MenuItem 
-                            onClick={loginModal.onOpen}
-                            label = "Login"
-                        />
-                        <MenuItem 
-                            onClick={registerModal.onOpen}
-                            label = "Sign Up"
-                        />
-                    </>
-                    )}
-                </div>
-            </div>
-        )}
+                    text-sm"
+        >
+          <div className="flex flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+                <MenuItem onClick={() => {}} label="Edit Profile" />
+                <hr />
+                <MenuItem onClick={() => signOut()} label="Log Out" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={loginModal.onOpen} label="Login" />
+                <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
+              </>
+            )}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default UserMenu
+export default UserMenu;
