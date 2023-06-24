d0e7da7f4a33b181c2d3ddd43a8a70dd17f5575b
JAEHO JEONG <e0960435@u.nus.edu>
2023-06-24 15:54:21 +0800

    Intermediate update with doctor registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import LoginModal from "../components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "../components/modals/RentModal";
 
 export const metadata = {
   title: "UHC@HOME",
@@ -25,6 +26,7 @@ export default async function RootLayout({
         <ClientOnly>
           <div className="">
             <ToasterProvider />
+            <RentModal />
             <LoginModal />
             <RegisterModal />
             <Nav currentUser={currentUser} />
