31fbd94bee0068eee56b3e7a11095cfb682e7ec7
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-06-29 19:23:36 +0800

    Changed about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import About from "../../components/About/About";
 
 export default function page() {
-  return <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+  return <main className="mx-auto max-w-full px-4 sm:px-6 ">
     <About />
   </main>;
 }
