7b8873fb97eb42d39bef54ba5dc239ff7383035a
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-06-29 21:29:43 +0800

    Routed Edit Profile to newly created ProfileModal

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,14 +1,11 @@
-import SignIn from "@components/auth/SignIn";
+
 import React from "react";
-import SignUp from "@components/auth/SignUp";
 
 const profile = () => {
   return (
     <>
       <br />
       <br />
-      <SignIn />
-      <SignUp />
     </>
   );
 };
