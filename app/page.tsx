1619bb054ef608d5c2390b2f62651345b274773d
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-07-01 21:24:40 +0800

    Reverse changes caused vercel to be unable to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import Categories from "../components/Navbar/Categories";
 
+export const dynamic = "force-dynamic";
 
 const Home = () => {
   return (
-    
     <section className="w-full flex-center flex-col">
-      <br /><br /><br />
+      <br />
+      <br />
+      <br />
       <h1 className="head_text text-center pinkyellow_gradient font-inter ">
         UHC@HOME
         <br />
-        <span className="greenblue_gradient">
-          BRINGING UHC TO YOUR HOME
-        </span>
+        <span className="greenblue_gradient">BRINGING UHC TO YOUR HOME</span>
       </h1>
       <p className="desc text-center">
         UHC@HOME is a tool that allows patients to be <br></br>
@@ -21,8 +21,8 @@ const Home = () => {
 
       {/* UHC pictures */}
       <div className="flex-between w-full mg-16 pt-3">
-        <img src="/assets/uhc1.png" alt="UHC picture" class="w-1/2"/>
-        <img src="/assets/uhc2.png" alt="UHC logo" class="w-1/2"/>
+        <img src="/assets/uhc1.png" alt="UHC picture" className="w-1/2" />
+        <img src="/assets/uhc2.png" alt="UHC logo" className="w-1/2" />
       </div>
     </section>
   );
