8af42307d64ba3f5871d57032065fb634fdfd3ed
JAEHO JEONG <e0960435@u.nus.edu>
2023-07-01 11:02:45 +0800

    deleted unnecessary props to Listing

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 
-import prisma from "../../../app/libs/prismadb" 
-import getCurrentUser from "../../../app/actions/getCurrentUser"
+import prisma from "../../../app/libs/prismadb";
+import getCurrentUser from "../../../app/actions/getCurrentUser";
 
-export async function POST(
-  request: Request, 
-) {
+export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
@@ -13,17 +11,7 @@ export async function POST(
   }
 
   const body = await request.json();
-  const { 
-    title,
-    description,
-    imageSrc,
-    category,
-    roomCount,
-    bathroomCount,
-    guestCount,
-    location,
-    price,
-   } = body;
+  const { title, description, imageSrc, category, location, price } = body;
 
   Object.keys(body).forEach((value: any) => {
     if (!body[value]) {
@@ -37,13 +25,10 @@ export async function POST(
       description,
       imageSrc,
       category,
-      roomCount,
-      bathroomCount,
-      guestCount,
       locationValue: location.value,
       price: parseInt(price, 10),
-      userId: currentUser.id
-    }
+      userId: currentUser.id,
+    },
   });
 
   return NextResponse.json(listing);
