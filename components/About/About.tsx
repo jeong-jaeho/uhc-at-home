525c5c8a5dcbd22fe718d893c1ba965a9b6c837b
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-07-08 00:54:47 +0800

    Made CreatorIntroduction component to reduce size of About.tsx

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,10 +5,29 @@ import jaeho from "../../public/assets/jaeho.jpg";
 import nat from "../../public/assets/nat.png";
 import Card from "../Card";
 import Link from "next/link";
+import CreatorIntroduction from "../CreatorIntroduction";
 
 const About = () => {
+  const jaehoImage = (
+    <Image
+      src={jaeho}
+      alt="picture of Jaeho"
+      width={325}
+      height={325}
+      className="rounded-full shadow-2xl"
+    />
+  );
+  const natImage = (
+    <Image
+      src={nat}
+      alt="picture of Nat"
+      width={325}
+      height={325}
+      className="rounded-full shadow-2xl"
+    />
+  );
   return (
-    <section className="w-full">
+    <div className="w-full">
       <p className="text-center text-2xl font-bold mb-10">
         Learn more about UHC@HOME
       </p>
@@ -21,12 +40,12 @@ const About = () => {
         <Card
           header="Our Project Log"
           content={
-            <div>
-              <Link
-                href="https://docs.google.com/document/d/1suMQAzlMDCM6jUfIWZu7__eeOEl6_sBdLZPNE8-vbx4/edit?usp=sharing"
-                className="text-sm"
-              >
-                To learn more about our project, please click here!
+            <div className="text-sm">
+              To learn more about our project, please click
+              <Link href="https://docs.google.com/document/d/1suMQAzlMDCM6jUfIWZu7__eeOEl6_sBdLZPNE8-vbx4/edit?usp=sharing">
+                <b>
+                  <em> here!</em>
+                </b>
               </Link>
             </div>
           }
@@ -35,12 +54,12 @@ const About = () => {
         <Card
           header="Our GitHub Repo"
           content={
-            <div>
-              <Link
-                href="https://github.com/itsNatTan/UHC-HOME"
-                className="text-sm"
-              >
-                To see the code for yourself, please click here!
+            <div className="text-sm">
+              To see the code for yourself, please click
+              <Link href="https://github.com/itsNatTan/UHC-HOME">
+                <b>
+                  <em> here!</em>
+                </b>
               </Link>
             </div>
           }
@@ -50,83 +69,23 @@ const About = () => {
       <p className="text-center text-2xl font-bold mt-20">
         Creators of UHC@HOME
       </p>
-      <div className="flex flex-row gap-32">
-        <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-          <div className="md:mt-2 md:w-1/2">
-            <Image
-              src={jaeho}
-              alt="picture of Jaeho"
-              width={325}
-              height={325}
-              className="rounded-full shadow-2xl"
-            />
-          </div>
-          <div className="md:mt-2 md:w-3/5">
-            <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
-              Hi, I&#39;m Jaeho Jeong!
-            </h1>
-            <p className="text-lg mt-4 mb-6 md:text-2xl">
-              I&#39;m a{" "}
-              <span className="font-semibold text-teal-600">
-                computer science student{" "}
-              </span>
-              at National University of Singapore.
-            </p>
-            <br />
-            I am interested in front-end development!
-            <br />
-            <a
-              href="https://github.com/jeong-jaeho"
-              className="w-full mg-16 pt-3"
-            >
-              <img
-                src="/assets/github.png"
-                width="70"
-                height="70"
-                className="hover:-translate-y-1 transition-transform cursor-pointer"
-              />
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-          <div className="md:mt-2 md:w-1/2">
-            <Image
-              src={nat}
-              alt="picture of Nat"
-              width={325}
-              height={325}
-              className="rounded-full shadow-2xl"
-            />
-          </div>
-          <div className="md:mt-2 md:w-3/5">
-            <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
-              Hi, I&#39;m Nathanael M. Tan!
-            </h1>
-            <p className="text-lg mt-4 mb-6 md:text-2xl">
-              I&#39;m a{" "}
-              <span className="font-semibold text-teal-600">
-                computer science student{" "}
-              </span>
-              at National University of Singapore.
-            </p>
-            <br />
-            I am interested in back-end development!
-            <br />
-            <a
-              href="https://github.com/itsNatTan"
-              className="w-full mg-16 pt-3"
-            >
-              <img
-                src="/assets/github.png"
-                width="70"
-                height="70"
-                className="hover:-translate-y-1 transition-transform cursor-pointer"
-              />
-            </a>
-          </div>
-        </div>
+      <div className="flex flex-row justify-items-center space-x-10">
+        <CreatorIntroduction
+          name="Jaeho Jeong"
+          occupation="Computer Science student"
+          interest="front-end development"
+          github="https://github.com/jeong-jaeho"
+          img={jaehoImage}
+        />
+        <CreatorIntroduction
+          name="Nathanael M. Tan"
+          occupation="Computer Science student"
+          interest="full-stack development"
+          github="https://github.com/itsNatTan"
+          img={natImage}
+        />
       </div>
-    </section>
+    </div>
   );
 };
 
---
+++ components/CreatorIntroduction.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+
+interface CreatorIntroductionProps {
+  name: string;
+  occupation: string;
+  interest: string;
+  github: string;
+  img: React.ReactNode;
+}
+
+const CreatorIntroduction: React.FC<CreatorIntroductionProps> = ({
+  img,
+  occupation,
+  interest,
+  github,
+  name
+}) => {
+  return (
+    <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+      <div className="md:mt-2 md:w-1/2">{img}</div>
+      <div className="md:mt-2 md:w-3/5">
+        <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
+          Hi, I&#39;m {name}!
+        </h1>
+        <p className="text-lg mt-4 mb-6 md:text-2xl">
+          I&#39;m a{" "}
+          <span className="font-semibold text-teal-600">
+            {occupation}{" "}
+          </span>
+          at National University of Singapore.
+        </p>
+        <br />
+        I am interested in {interest}!
+        <br />
+        <a href={github} className="w-full mg-16 pt-3">
+          <img
+            src="/assets/github.png"
+            width="70"
+            height="70"
+            className="hover:-translate-y-1 transition-transform cursor-pointer"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CreatorIntroduction;
