e29b35ec14d81723aaf8be183ee479a62eae2009
Nathanael M. Tan <nathanaelmark.tan@gmail.com>
2023-07-09 13:21:24 +0800

    Added padding for some pages

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Card from "../../components/Card";
 import CreatorIntroduction from "../../components/CreatorIntroduction";
-import jaeho from "../../public/assets/jaeho.jpg"
-import nat from "../../public/assets/nat.png"
+import jaeho from "../../public/assets/jaeho.jpg";
+import nat from "../../public/assets/nat.png";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -26,67 +26,72 @@ export default function page() {
     />
   );
   return (
-  <main className="mx-auto max-w-full px-8 sm:px-10">
-    <div className="w-full">
-      <p className="text-center text-2xl font-bold mb-10">
-        Learn more about UHC@HOME
-      </p>
-      <div className="flex flex-row justify-center text-center gap-20">
-        <Card
-          header="Our Motivations"
-          content={<div className="text-sm">Queue too long :(</div>}
-          hasButton={false}
-        ></Card>
-        <Card
-          header="Our Project Log"
-          content={
-            <div className="text-sm">
-              To learn more about our project, please click
-              <Link href="https://docs.google.com/document/d/1suMQAzlMDCM6jUfIWZu7__eeOEl6_sBdLZPNE8-vbx4/edit?usp=sharing">
-                <b>
-                  <em> here!</em>
-                </b>
-              </Link>
-            </div>
-          }
-          hasButton={false}
-        ></Card>
-        <Card
-          header="Our GitHub Repo"
-          content={
-            <div className="text-sm">
-              To see the code for yourself, please click
-              <Link href="https://github.com/itsNatTan/UHC-HOME">
-                <b>
-                  <em> here!</em>
-                </b>
-              </Link>
-            </div>
-          }
-          hasButton={false}
-        ></Card>
+    <main className="mx-auto max-w-full px-6 sm:px-10">
+      <div className="w-full">
+        <p className="text-center text-2xl font-bold mb-10">
+          Learn more about UHC@HOME
+        </p>
+        <div className="flex flex-row justify-center text-center gap-20">
+          <Card
+            header="Our Motivations"
+            content={<div className="text-sm">Queue too long :(</div>}
+            hasButton={false}
+          ></Card>
+          <Card
+            header="Our Project Log"
+            content={
+              <div className="text-sm">
+                To learn more about our project, please click
+                <Link
+                  href="https://docs.google.com/document/d/1suMQAzlMDCM6jUfIWZu7__eeOEl6_sBdLZPNE8-vbx4/edit?usp=sharing"
+                  className="text-blue-400"
+                >
+                  <b>
+                    <em> here!</em>
+                  </b>
+                </Link>
+              </div>
+            }
+            hasButton={false}
+          ></Card>
+          <Card
+            header="Our GitHub Repo"
+            content={
+              <div className="text-sm">
+                To see the code for yourself, please click
+                <Link
+                  href="https://github.com/itsNatTan/UHC-HOME"
+                  className="text-blue-400"
+                >
+                  <b>
+                    <em> here!</em>
+                  </b>
+                </Link>
+              </div>
+            }
+            hasButton={false}
+          ></Card>
+        </div>
+        <p className="text-center text-2xl font-bold mt-20">
+          Creators of UHC@HOME
+        </p>
+        <div className="flex flex-row justify-items-center space-x-10">
+          <CreatorIntroduction
+            name="Jaeho Jeong"
+            occupation="Computer Science student"
+            interest="front-end development"
+            github="https://github.com/jeong-jaeho"
+            img={jaehoImage}
+          />
+          <CreatorIntroduction
+            name="Nathanael M. Tan"
+            occupation="Computer Science student"
+            interest="full-stack development"
+            github="https://github.com/itsNatTan"
+            img={natImage}
+          />
+        </div>
       </div>
-      <p className="text-center text-2xl font-bold mt-20">
-        Creators of UHC@HOME
-      </p>
-      <div className="flex flex-row justify-items-center space-x-10">
-        <CreatorIntroduction
-          name="Jaeho Jeong"
-          occupation="Computer Science student"
-          interest="front-end development"
-          github="https://github.com/jeong-jaeho"
-          img={jaehoImage}
-        />
-        <CreatorIntroduction
-          name="Nathanael M. Tan"
-          occupation="Computer Science student"
-          interest="full-stack development"
-          github="https://github.com/itsNatTan"
-          img={natImage}
-        />
-      </div>
-    </div>
-  </main>
-  )
+    </main>
+  );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Categories from "../components/Navbar/Categories";
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
-      <br />
+    <section className="w-full flex-center flex-col px-4 sm:px-10">
       <h1 className="head_text text-center pinkyellow_gradient font-inter ">
         UHC@HOME
         <br />
@@ -16,7 +15,7 @@ const Home = () => {
       </p>
 
       {/* UHC pictures */}
-      <div className="flex-between w-full mg-16 pt-3">
+      <div className="flex-between pt-10">
         <img src="/assets/uhc1.png" alt="UHC picture" className="w-1/2" />
         <img src="/assets/uhc2.png" alt="UHC logo" className="w-1/2" />
       </div>
